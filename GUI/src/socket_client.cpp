8aee05cf30e71b4f55ff5780e43ee000918ee7e4
nathan03VT <nathan03@vt.edu>
2023-09-16 10:46:28 -0400

    Temp Socket Connect Json

--- GUI/src/socket_client.cpp
+++ GUI/src/socket_client.cpp
@@ -2,48 +2,80 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <string>
+#include <cstdlib>
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+int clientSocket; // Declare the socket as a global variable
+
+// Function to send a JSON message to the server
+void sendJSONMessage(const json& messageJson) {
+    std::string jsonMessage = messageJson.dump();
+    send(clientSocket, jsonMessage.c_str(), jsonMessage.size(), 0);
+}
 
 int main() {
-    // Create a socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == -1) {
-        std::cout << "Failed to create connection to client socket connection" << std::endl;
-        return -1;
-    }
-    // Define the server side address
-    struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_LOOPBACK;
-
-    // Connect to Python Server
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cout << "Failed to Connect to Server" << std::endl;
-        return -1;
-    }
-
-    // Send Test Message
-    const char* message = "Hello Python Server!";
-    ssize_t bytesSent = send(clientSocket, message, strlen(message), 0);
-
-    if (bytesSent == -1) {
-        std::cout << "Failed to send message" << std::endl;
-        return -1;
-    }
-
-    std::cout << "SENT DATA SUCCESSFUL" << std::endl;
-
-    // Receive and display the response from the server
+    struct sockaddr_in serverAddr;
     char buffer[1024];
-    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-    if (bytesReceived == -1) {
-        std::cout << "Failed to receive response" << std::endl;
-    } else {
-        buffer[bytesReceived] = '\0'; // Null-terminate the received data
-        std::cout << "Received from server: " << buffer << std::endl;
+
+    // Create a socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "Error: Could not create socket" << std::endl;
+        return 1;
     }
 
+    // Configure server address
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080); // Replace with your server's port
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Replace with your server's IP address
+
+    // Connect to the server
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error: Could not connect to server" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Connected to server. Type 'exit' to quit." << std::endl;
+
+    // Create a connection JSON object
+    json connectionJson;
+    connectionJson["type"] = "connection";
+    connectionJson["client_name"] = "YourClientName"; // Replace with your client name or identifier
+
+    // Send the connection JSON to the server
+    sendJSONMessage(connectionJson);
+
+    while (true) {
+        // Read input from the terminal
+        std::cout << "Enter a message (JSON format): ";
+        std::string message;
+        std::getline(std::cin, message);
+
+        if (message == "exit") {
+            break;
+        }
+
+        // Create a JSON object for the user message
+        json messageJson;
+        messageJson["type"] = "user_message";
+        messageJson["content"] = message;
+
+        // Send the user message as JSON to the server using the external function
+        sendJSONMessage(messageJson);
+
+        // Receive and display the server's response
+        memset(buffer, 0, sizeof(buffer));
+        recv(clientSocket, buffer, sizeof(buffer), 0);
+        std::cout << "Server response: " << buffer << std::endl;
+    }
+
+    // Close the socket
     close(clientSocket);
+
     return 0;
 }
